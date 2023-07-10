f329f3ad873085f0a4ada8a469826f044596fc4f
cuong404s <90568573+cuong404s@users.noreply.github.com>
2023-07-10 12:18:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: '🎉Happy Birthday🎉',
+    introDesc: `Chúc mừng sinh nhật cậu. Cô gái mạnh mẽ, ấm áp và rấc rấc đáng iu  :3 `,
+    btnIntro: 'Tu bi con tờ niu...',
+    title: 'Bước sang tuổi mới, điều em mong muốn là gì ?',
+    desc: '14/7 14/7 14/7 những điều đặc biệt thường nhắc lại 3 lần :>',
+    btnYes: 'Điều em mong muốn là...',
+    btnNo: 'Không, Em chẳng nói :3',
+    question:'Úm ba la mọi điều mong ước sẽ thành hiện thực 🫶',
+    btnReply: 'Gửi nè <3',
+    reply: 'Chúc em tuổi 19 thật nhiều niềm vui, hạnh phúc và xinh đẹp hơn nữa. Mọi điều tốt đẹp nhất sẽ đến với em. Chúc mừng sinh nhật em 🎉',
+    mess: 'Cảm ơn em đã xuất hiện trong cuộc đời này',
+    messDesc: 'Người con gái không chỉ biết lắng nghe, sẻ chia yêu thương mà còn vô cùng mạnh mẽ và tràn đầy nguồn năng lượng tích cực. Với anh, em vẫn luôn đặc biệt như thế, vẫn luôn là một và là duy nhất. Hãy viết tiếp thêm nhiều kỉ niệm đẹp cùng nhau nữa nhá !',
+    btnAccept: 'Vâng ạ <3',
+    messLink: 'm.me/cuong404.vn' 
+}
